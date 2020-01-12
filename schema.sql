ae82364ea714be16e3c0acff0893817744065353
Matthew Knight <mattknight.sa@gmail.com>
2020-01-12 15:04:13 -0600

    updated sql

--- schema.sql
+++ schema.sql
@@ -3,6 +3,19 @@ CREATE database employee_trackerDB;
 
 USE employee_trackerDB;
 
+CREATE TABLE department (
+ id INT NOT NULL AUTO_INCREMENT,
+ name VARCHAR(30) NULL,
+ PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30) NULL,
+  salary Decimal(10,4) NULL,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NULL,
