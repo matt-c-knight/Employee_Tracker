f4a9b5d5c44000969c69ab142492f1b2484dbdcc
Matthew Knight <mattknight.sa@gmail.com>
2020-01-12 15:55:54 -0600

    adding inquirer

--- server.js
+++ server.js
@@ -3,18 +3,19 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
-
-  
   port: 3306,
-
   user: "root",
-
- 
   password: "",
-  database: ""
+  database: "employee_trackerDB"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
-  
+  start();
 });
+function start() {
+    inquirer
+     .prompt({
+         
+     })
+}
