b65b06f81a9fc36655e67a1236de1cdfcc875c06
Matthew Knight <mattknight.sa@gmail.com>
2020-01-12 16:45:47 -0600

    adding to inquirer

---
+++ functions.js
@@ -0,0 +1,7 @@
+function addDepartment() {
+
+}
+
+function addRole() {
+    
+}
--- server.js
+++ server.js
@@ -16,6 +16,29 @@ connection.connect(function(err) {
 function start() {
     inquirer
      .prompt({
-         
+       name: "options",
+       type: "list",
+       message: "Please choose from the options below to add, view, or update:",
+       choices: ["Add department", "Add role", "Add employee", "View department", "View role", "View employee", "Update employee role", "Exit"]
+     })
+     .then(function(answer) {
+         if(answer.options === "Add department") {
+             addDepartment();
+         }
+         else if(answer.options === "Add role") {
+             addRole();
+         }
+         else if(answer.options === "Add employee") {
+             addEmployee();
+         }
+         else if(answer.options === "View department") {
+             viewDepartment();
+         }
+         else if(answer.options === "View role") {
+             viewRole();
+         }
+         else if(answer.options === "View employee") {
+             viewEmployee();
+         }
      })
 }
