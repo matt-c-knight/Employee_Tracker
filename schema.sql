cf3f8fab3f0b5a319b8189bc5ebf11c07f87a7e8
Matthew Knight <mattknight.sa@gmail.com>
2020-02-22 19:03:41 -0600

    updated schema

--- schema.sql
+++ schema.sql
@@ -5,35 +5,27 @@ USE employee_trackerDB;
 
 CREATE TABLE department (
  id INT NOT NULL AUTO_INCREMENT,
- name VARCHAR(30) NULL,
+ name VARCHAR(30) NOT NULL,
  PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
   id INT NOT NULL AUTO_INCREMENT,
-  title VARCHAR(30) NULL,
-  salary Decimal(10,4) NULL,
+  title VARCHAR(30) NOT NULL,
+  salary Decimal(10,4) NOT NULL,
   department_id INT,
   PRIMARY KEY (id),
-  CONSTRAINT FK_department FOREIGN KEY (department_id) REFERENCES department(id)
+  FOREIGN KEY(department_id) REFERENCES department (id)
 );
 
 CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NULL,
-  last_name VARCHAR(30) NULL,
-  role_id INT NULL,
-  manager_id INT NULL,
-  PRIMARY KEY (id)
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
 
 
--- W3 Schools 
--- CREATE TABLE Orders (
---     OrderID int NOT NULL,
---     OrderNumber int NOT NULL,
---     PersonID int,
---     PRIMARY KEY (OrderID),
---     CONSTRAINT FK_PersonOrder FOREIGN KEY (PersonID)
---     REFERENCES Persons(PersonID)
--- );
